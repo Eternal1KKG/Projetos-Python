9a05fc96ded826dca0669514acab59f684f415d0
Everton <everton.aocontato@gmail.com>
2024-08-26 21:57:11 -0300

    exercicio repetição

--- Seleção/exercicio-10.py
+++ Seleção/exercicio-10.py
@@ -1,14 +1,26 @@
-#Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e
-#escrevê-los em ordem crescente.
+# 10) Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
 
-#entrada
-valor1 = float(input("Digite um valor: ").replace(',','.'))
-valor2 = float(input("Digite um segundo valor: ").replace(',','.'))
-valor3 = float(input("Digite um terceiro valor: ").replace(',','.'))
-valores = [valor1, valor2, valor3]
+# entrada
+valores = []
+valores.append(int(input("Digite o primeiro valor?")))
+valores.append(int(input("Digite o segundo valor?")))
+valores.append(int(input("Digite o terceiro valor?")))
 
 #processamento
-valores.sort()
+if (valores[0] > valores[1]):
+    valorTemp = valores[0]
+    valores[0] = valores[1]
+    valores[1] = valorTemp
 
-#saida
-print("Esses são seus valores em ordem crescente: ", valores)
+if (valores[0] > valores[2]):
+    valorTemp = valores[0]
+    valores[0] = valores[2]
+    valores[2] = valorTemp
+
+if (valores[1] > valores[2]):
+    valorTemp = valores[1]
+    valores[1] = valores[2]
+    valores[2] = valorTemp  
+
+for x in valores:
+    print(x) #saida
